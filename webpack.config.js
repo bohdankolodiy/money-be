af550bd8632fb05945b082d416f370d28f563289
bkolodiy <bkolodiy@binariks.com>
2024-06-19 11:22:14 +0300

    Add ingmoring plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
+const path = require('path');
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV == "production";
-
-const config = {
-  entry: "./src/app.ts",
+module.exports = {
+  mode: 'production',
+  entry: '/src/app.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
   },
   devServer: {
     open: true,
@@ -19,10 +18,14 @@ const config = {
     port: 3000,
   },
   plugins: [
+
+    new webpack.IgnorePlugin({
+      resourceRegExp: /mapbox/
+    }),
+
     new webpack.IgnorePlugin({
       resourceRegExp: /^pg-native$/,
     }),
-    new HtmlWebpackPlugin(),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
@@ -32,20 +35,14 @@ const config = {
         test: /\.(ts|tsx)$/i,
         loader: "ts-loader",
         exclude: [
-          /\/node_modules/,
           path.resolve(__dirname, "node_modules"),
           path.resolve(__dirname, "../node_modules"),
-          /\.js$/,
-          /\.html$/,
         ],
       },
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", "..."],
   },
   target: "node", // Add this line to target a Node.js environment,
   externals: {
@@ -55,20 +52,8 @@ const config = {
     "aws-sdk": "commonjs aws-sdk",
     nock: "commonjs nock",
     "mock-aws-s3": "commonjs mock-aws-s3",
-    "node-gyp": "commonjs node-gyp",
     "node-pre-gyp": "commonjs node-pre-gyp",
-    "mapbox": "commonjs @mapbox",
-    "npm": "commonjs npm",
 
     // Add other modules as needed
   },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
 };
